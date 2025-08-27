bde0036b56b3738eedf4548be170c31eb2d3635e
Maliuk-Olexandr <maamcd100@gmail.com>
2025-08-27 03:37:13 +0300

    Update image sources and improve semantic structure in HTML and CSS

--- src/js/3-how-to-play.js
+++ src/js/3-how-to-play.js
@@ -1,19 +1,10 @@
-export function debounce(func, delay) {
-  let timeout;
-  return (...args) => {
-    clearTimeout(timeout);
-    timeout = setTimeout(() => func(...args), delay);
-  };
-}
 
 import Swiper from 'swiper';
 import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 
-document.addEventListener('DOMContentLoaded', () => {
   const swiper = new Swiper('#how-to-play-swiper', {
     modules: [Navigation, Pagination],
     speed: 600,
@@ -42,10 +33,3 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     loop: true,
   });
-
-  const handleResize = debounce(() => {
-    swiper.update();
-  }, 300);
-
-  window.addEventListener('resize', handleResize);
-});
